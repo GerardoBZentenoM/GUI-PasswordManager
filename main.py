566b4916349471d1f13a2bfa249d803d45398aef
GerardoBZentenoM <brallanzenteno@gmail.com>
2022-07-14 12:03:17 -0500

    added assets and window

---
+++ main.py
@@ -0,0 +1,19 @@
+import tkinter
+
+# ---------------------------- PASSWORD GENERATOR ---------------------------#
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = tkinter.Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = tkinter.Canvas(width=200, height=224, highlightthickness=0)
+logo_img = tkinter.PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+
+canvas.grid(column=1, row=1)
+
+window.mainloop()
