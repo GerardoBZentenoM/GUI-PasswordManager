04cd85a302d441002bec5d8bba600bc905294fd6
GerardoBZentenoM <brallanzenteno@gmail.com>
2022-07-15 19:42:22 -0500

    correctios to gui

--- main.py
+++ main.py
@@ -108,14 +108,14 @@ label_username.grid(column=0, row=2)
 label_password = tkinter.Label(text="Password: ")
 label_password.grid(column=0, row=3)
 
-entry_website = tkinter.Entry(width=44)
-entry_website.grid(column=1, row=1, columnspan=2)
+entry_website = tkinter.Entry(width=21)
+entry_website.grid(column=1, row=1, columnspan=1)
 
-entry_username = tkinter.Entry(width=44)
+entry_username = tkinter.Entry(width=42)
 entry_username.grid(column=1, row=2, columnspan=2)
 entry_username.insert(0, "brallanzenteno@gmail.com")
 
-entry_password = tkinter.Entry(width=25)
+entry_password = tkinter.Entry(width=21)
 entry_password.grid(column=1, row=3)
 
 button_generate_password = tkinter.Button(
@@ -125,7 +125,7 @@ button_generate_password.grid(column=2, row=3)
 button_add_password = tkinter.Button(text="Add", width=41, command=save)
 button_add_password.grid(column=1, row=4, columnspan=2)
 
-search_button = tkinter.Button(text="Search", width=13, command=find_password)
+search_button = tkinter.Button(text="Search", width=16, command=find_password)
 search_button.grid(row=1, column=2)
 
 
